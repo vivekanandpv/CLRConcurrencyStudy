bc35057f39b6d1f259735f775701fef9214cdbd2
vivek <vivek@athenaeum.in>
2021-04-04 17:27:27 +0530

    done

--- Program.cs
+++ Program.cs
@@ -7,7 +7,17 @@ namespace CLRConcurrencyStudy.Fundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            //  CLR starts the process with a thread called main thread, usually with id 1
+            Console.WriteLine($"Running on {Thread.CurrentThread.ManagedThreadId}");
+            
+            //  This current thread is right now in Running state (doing computation)
+            Console.WriteLine($"Current status {Thread.CurrentThread.ThreadState}");
+            
+            //  Thread can be blocked by relinquishing the execution time
+            Thread.Sleep(1000);
+            
+            //  Below line is executed after the thread resumes execution
+            Console.WriteLine($"Resuming operation of {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
