616e29d3f002613c1b7797804a5ebe65ef7c8259
vivek <vivek@athenaeum.in>
2021-04-05 22:46:13 +0530

    done

--- Program.cs
+++ Program.cs
@@ -7,30 +7,24 @@ namespace CLRConcurrencyStudy.Fundamentals
     {
         static void Main(string[] args)
         {
-            //  We can set a name for a thread
-            //  But cannot change it once the thread starts executing
-            var thread1 = new Thread(() =>
+            Console.WriteLine("Main thread starting...");
+            
+            var thread = new Thread(() =>
             {
-                Console.WriteLine($"Starting {Thread.CurrentThread.Name}");
-                
-                //  A thread created this way is a foreground thread
-                //  Application won't exit unless all foreground threads
-                //  have finished executing
-                Console.WriteLine($"{Thread.CurrentThread.Name} is foreground: {!Thread.CurrentThread.IsBackground}");
+                Console.WriteLine($"{Thread.CurrentThread.Name} starting...");
+                Thread.Sleep(2000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} completing...");
+            }){Name = "New Thread"};
 
-                //  Blocking: we relinquish the CPU time for the specified timeout
-                //  During this interval, the thread consumes no CPU cycles
-                //  But the thread is alive (not finished) nonetheless
-                Console.WriteLine($"{Thread.CurrentThread.Name} is alive: {Thread.CurrentThread.IsAlive}");
-                Thread.Sleep(5000);
-                Console.WriteLine($"{Thread.CurrentThread.Name} completing");
-            }) {Name = "New thread 1"};
-         
-            thread1.Start();
+            thread.Start();
 
-            //  Though this looks like the end of the program,
-            //  it isn't. 
-            Console.WriteLine("Main thread completing");
+            //  We block the main thread here
+            //  We wait (without wasting CPU cycles) till thread finishes
+            //  Without thread.Join(); execution should have proceed to the next
+            //  instruction in the main thread (to "Main thread resumes...")
+            thread.Join();  
+
+            Console.WriteLine("Main thread resumes...");
         }
     }
 }
