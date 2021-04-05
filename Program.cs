5fb24432b6e46f23bc63917c44af48b1e5c39d2d
vivek <vivek@athenaeum.in>
2021-04-05 23:31:07 +0530

    done

--- Program.cs
+++ Program.cs
@@ -8,17 +8,29 @@ namespace CLRConcurrencyStudy.Fundamentals
     {
         static void Main(string[] args)
         {
-            var backgroundThread = new Thread(() =>
+            for (int i = 0; i < 1_000_000_000; i++)
             {
-                Console.WriteLine($"{Thread.CurrentThread.Name} executing");
-                //  Application doesn't wait for this blocking as this is a background thread
-                Thread.Sleep(10000);
-                Console.WriteLine($"{Thread.CurrentThread.Name} finishes");
-            }) {Name = "Background thread", IsBackground = true};
-            
-            backgroundThread.Start();
-
-            Console.WriteLine("Main thread exits...");
+                if (i == 1)
+                {
+                    var thread = new Thread(() =>
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.Name} starting");
+                        Thread.Sleep(100);
+                        //  this lambda expression captures the local variable i
+                        //  that is defined and modified in the outer scope
+                        //  because of the modification in the outer scope
+                        //  the exact value of i here is non-deterministic
+                        
+                        //  This happens because, it is the same memory location that
+                        //  gets overwritten with new values for i
+                        
+                        //  Access to i is permitted in C#. In Java, this is prohibited.
+                        Console.WriteLine($"{Thread.CurrentThread.Name}: i = {i}");
+                    }) {Name = "New Thread"};
+                    
+                    thread.Start();
+                }
+            }
         }
     }
 }
