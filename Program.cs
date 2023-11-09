54e3c14818afbea53599f0453664c350b38b272e
Vivekanand P V <vivekanandpv@gmail.com>
2023-11-09 11:00:35 +0530

    corrected

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 
@@ -9,6 +10,7 @@ namespace CLRConcurrencyStudy.Fundamentals
         static void Main(string[] args)
         {
             var counter = new NonSynchronizedCounter();
+            List<Thread> threads = new List<Thread>();
             
             for (var i = 0; i < 10; ++i)
             {
@@ -21,9 +23,13 @@ namespace CLRConcurrencyStudy.Fundamentals
                         counter.Decrement();
                     }
                 });
-                
-                thread.Start();
+
+                threads.Add(thread);
             }
+
+            threads.ForEach(t => t.Start());
+
+            threads.ForEach(t => t.Join());
 
             //  We expect zero, but we get...
             Console.WriteLine($"Final value: {counter.Value}");
@@ -37,13 +43,14 @@ namespace CLRConcurrencyStudy.Fundamentals
         public void Increment()
         {
             //  This is a non atomic operation
-            //  Has 3 parts: read, increment, and write operations
+            //  Has 3 parts: read, operate, and assign operations
             ++Value;
         }
 
         public void Decrement()
         {
             //  This too is non-atomic for the same reason
+            //  Has 3 parts: read, operate, and assign operations
             --Value;
         }
     }
